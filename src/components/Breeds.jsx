bdde5b39410a2ca844bbdf04f4797a27a3dc29c4
Tatiana LLorente <tatianallorente@gmail.com>
2023-04-07 02:46:49 +0200

    Migrate to Vite

--- src/components/Breeds.jsx
+++ src/components/Breeds.jsx
@@ -7,23 +7,12 @@ import Pagination from './Pagination'
 import { useFetchBreeds } from '../hooks/useFetchBreeds'
 
 const Breeds = ({searchBreed}) => {
-  // Search
-  const [searchResults, setSearchResults] = useState([]);
 
   // Pagination
   const [currentPage, setCurrentPage] = useState(0);
 
   // Get breeds
-  const { breeds, totalPages, loadingBreeds:loading } = useFetchBreeds(currentPage, searchBreed);
-
-
-  // Get breeds from search
-  useEffect(() => {
-    // Case insensitive
-    if (Object.keys(searchBreed)?.length > 0 && searchBreed?.search_breed !== '') {
-      setSearchResults(breeds?.filter(breed => breed.name.toLowerCase().includes(searchBreed.search_breed.toLowerCase())));
-    }
-  }, [searchBreed]);
+  const { breeds, searchResults, totalPages, loadingBreeds:loading } = useFetchBreeds(currentPage, searchBreed);
 
 
   const handlePagination = (e) => {
@@ -36,15 +25,13 @@ const Breeds = ({searchBreed}) => {
       {loading
         ? <Spinner/>
         : Object.keys(searchBreed)?.length > 0 && searchBreed?.search_breed !== ''
-          ? (
-            searchResults.length > 0
+          ? searchResults.length > 0
             ? <div className="breeds-cards">
                 {searchResults.map(filteredBreed => (
                   <Breed key={filteredBreed.id} breed={filteredBreed} />
                 ))}
               </div>
             : <Error msg="No se encontraron resultados"/>
-          )
           : <>
               <Pagination
                 totalPages={totalPages}
@@ -69,3 +56,35 @@ const Breeds = ({searchBreed}) => {
 }
 
 export default Breeds;
+
+/*
+      {loading
+        ? <Spinner/>
+        : Object.keys(searchBreed)?.length > 0 && searchBreed?.search_breed !== ''
+          ? searchResults.length > 0
+            ? <div className="breeds-cards">
+                {searchResults.map(filteredBreed => (
+                  <Breed key={filteredBreed.id} breed={filteredBreed} />
+                ))}
+              </div>
+            : <Error msg="No se encontraron resultados"/>
+          : <>
+              <Pagination
+                totalPages={totalPages}
+                handlePagination={handlePagination}
+                currentPage={currentPage}
+              />
+              <div className="breeds-cards">
+                {breeds.map( breed => (
+                  <Breed key={breed.id} breed={breed} />
+                ))}
+              </div>
+              <Pagination
+                totalPages={totalPages}
+                handlePagination={handlePagination}
+                currentPage={currentPage}
+                bottom={true}
+              />
+            </>
+      }
+*/
--- src/hooks/useFetchBreeds.jsx
+++ src/hooks/useFetchBreeds.jsx
@@ -5,22 +5,47 @@ import { getBreeds } from '../services'
 
 export const useFetchBreeds = (currentPage, searchBreed) => {
 
-	const [state, setState] = useState({ breeds: [], totalPages: 1, loadingBreeds: true });
+  const initialState = {breeds: [], searchResults: [], totalPages: 1, loadingBreeds: true};
+
+	const [state, setState] = useState(initialState);
 
   const pageLimit = 9; // Mostrar 9 elementos en cada página
 
 
   useEffect(() => {
+    if (Object.keys(searchBreed)?.length > 0 && searchBreed?.search_breed !== '') {
+      setState(initialState);
+
+      getBreeds()
+        .then(breeds => {
+          // Case insensitive
+          const results = breeds?.filter(breed =>
+            breed.name.toLowerCase().includes(searchBreed.search_breed.toLowerCase())
+          );
+
+          setState({
+            ...state,
+            searchResults: results,
+            loadingBreeds: false
+          });
+        })
+    }
+  }, [searchBreed]);
+
+  useEffect(() => {
+    setState(initialState);
+
     getBreeds(currentPage, pageLimit)
       .then(breeds => {
         const {paginationCount} = breeds;
         setState({
-          breeds: breeds,
+          ...state,
+          breeds,
           totalPages: Math.ceil(paginationCount / pageLimit),
           loadingBreeds: false
         });
       })
-  }, [currentPage, searchBreed]);
+  }, [currentPage]);
 
   return state;
 }
